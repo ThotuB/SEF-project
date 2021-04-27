449db87887cbe85f9fca39b63891ffd9fc48a447
bogdantatu10@gmail.com <bogdantatu10@gmail.com>
2021-04-27 22:24:09 +0300

    ayoo

--- sef-project/src/main/java/Login.java
+++ sef-project/src/main/java/Login.java
@@ -7,51 +7,118 @@ class LoginFrame extends JFrame implements ActionListener {
     Container container = getContentPane();
 
     JLabel loginLabel = new JLabel("Login", SwingConstants.CENTER);
-    JLabel usernameLabel = new JLabel("Username: ");
-    JLabel passwordLabel = new JLabel("Password: ");
+    JLabel usernameLabel = new JLabel("Username", SwingConstants.CENTER);
+    JLabel passwordLabel = new JLabel("Password", SwingConstants.CENTER);
     JTextField usernameField = new JTextField();
     JPasswordField passwordField = new JPasswordField();
-    JButton loginButton = new JButton("LOGIN");
     JCheckBox showPasswordCheckBox = new JCheckBox("Show Password");
+    JButton loginButton = new JButton("LOGIN");
+    JButton registerButton = new JButton("CREATE ACCOUNT");
 
-    LoginFrame(){
+    LoginFrame() {
         resetContainerLayout();
         setComponentProperties();
         setContainerComponents();
+        setActionEvents();
     }
     
     public void resetContainerLayout(){
         container.setLayout(null);
     }
 
-    public void setComponentProperties(){
+    public void setComponentProperties() {
         loginLabel.setBounds(0, 20, 400, 100);
         loginLabel.setFont(new Font("Verdana", Font.PLAIN, 50));
-//        usernameLabel.setBounds();
-//        passwordLabel.setBounds();
-//        usernameField.setBounds();
-//        passwordField.setBounds();
-//        loginButton.setBounds();
-//        showPasswordCheckBox.setBounds();
+
+        usernameLabel.setBounds(0, 100, 400, 100);
+        usernameLabel.setFont(new Font("Verdana", Font.PLAIN, 15));
+        usernameField.setBounds(50, 170, 300, 50);
+
+        passwordLabel.setBounds(0, 200, 400, 100);
+        passwordLabel.setFont(new Font("Verdana", Font.PLAIN, 15));
+        passwordField.setBounds(50, 270, 300, 50);
+
+        showPasswordCheckBox.setBounds(50, 330, 200, 15);
+        loginButton.setBounds(50, 400, 100,50);
+        registerButton.setBounds(200, 400, 150, 50);
     }
 
-    public void setContainerComponents(){
+    public void setContainerComponents() {
         container.add(loginLabel);
         container.add(usernameLabel);
         container.add(passwordLabel);
         container.add(usernameField);
         container.add(passwordField);
         container.add(loginButton);
+        container.add(registerButton);
         container.add(showPasswordCheckBox);
+    }
+
+    public void setActionEvents() {
+        loginButton.addActionListener(this);
+        registerButton.addActionListener(this);
+        showPasswordCheckBox.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if ( e.getSource() == showPasswordCheckBox ){
+            if ( showPasswordCheckBox.isSelected() ){
+                passwordField.setEchoChar((char) 0);
+            }
+            else {
+                passwordField.setEchoChar('*');
+            }
+        }
+        if ( e.getSource() == loginButton ){
+            String usernameText;
+            char[] passwordText = new char[20];
 
+            usernameText = usernameField.getText();
+            passwordText = passwordField.getPassword();
+
+            if ( Login.loginTest(usernameText, passwordText) ){
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            }
+            else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+        }
+        if ( e.getSource() == registerButton ){
+            String usernameText;
+            char[] passwordText = new char[20];
+
+            usernameText = usernameField.getText();
+            passwordText = passwordField.getPassword();
+
+            if ( !Login.usernameTest(usernameText) ){
+                JOptionPane.showMessageDialog(this, "Registration Complete");
+            }
+            else {
+                JOptionPane.showMessageDialog(this, "Username Already Taken");
+            }
+        }
     }
 }
 
 public class Login {
+    public static boolean loginTest(String username, char[] password){
+        if ( usernameTest(username) ){
+            return passwordTest(password);
+        }
+        return false;
+    }
+
+    public static boolean passwordTest(char[] password) {
+        String passwordString = new String(password);
+
+        return passwordString.equals("12345");
+    }
+
+    public static boolean usernameTest(String username) {
+        return username.equals("Bob");
+    }
+
     public static void main(String[] args){
         LoginFrame frame = new LoginFrame();
 
