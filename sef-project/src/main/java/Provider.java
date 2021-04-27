02087f8768d329d1341a68fa21d4976198c8fde5
RaulQX <raul.sarbu@student.upt.ro>
2021-04-27 23:25:11 +0300

    added json dtb

--- sef-project/src/main/java/Provider.java
+++ sef-project/src/main/java/Provider.java
@@ -1,8 +1,70 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class Provider {
 
     private String name;
     private ArrayList<Game> games;
+
+    public Provider (String name)  {
+        this.name = name;
+        games = new ArrayList<>();
+    }
+
+    public void addGame (Game g) {
+        games.add(g);
+    }
+
+    @Override
+    public String toString() {
+        return "Provider{" +
+                "name='" + name + '\'' +
+                ", games=" + games +
+                '}';
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void printJson(String filename) throws IOException {
+
+        JSONObject jobj = new JSONObject();
+        JSONArray ja = new JSONArray();
+
+        Map<String, java.io.Serializable> m = new LinkedHashMap<>(5);
+
+        for (Game g: this.games) {
+            m.put("name", g.getName());
+            m.put("price", g.getPrice());
+            m.put("description", g.getDescription());
+            m.put("startDate", g.getStartDate());
+            m.put("endDate", g.getEndDate());
+        }
+
+        ja.put(m);
+
+        jobj.put("name", this.name);
+        jobj.put("games", ja);
+
+        Files.write(Paths.get(filename), jobj.toString().getBytes());
+
+        PrintWriter pw = new PrintWriter("ceva.json");
+        pw.write(jobj.toString());
+
+        pw.flush();
+        pw.close();
+
+    }
+
 
 }
--- sef-project/src/main/java/TestClass.java
+++ sef-project/src/main/java/TestClass.java
@@ -1,12 +1,21 @@
-import java.text.ParseException;
+import java.io.IOException;
 
 public class TestClass {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        Game x = new Game("E", 12, "descr", "12-12-2012", "31-01-2929", 1);
+        Game x = new Game("E", 12, "descr", "12-12-2012", "31-01-2929", true);
+        Game y = new Game("E", 12, "descr", "12-12-2012", "31-01-2929", true);
 
-        System.out.println(x);
+
+
+        Provider e = new Provider("name1");
+        e.addGame(x);
+        e.addGame(y);
+
+        System.out.println(e);
+
+        e.printJson("dtb_resources\\test2.json");
 
     }
 
