e338bed598c5ce3915086f19fd24fd3b68227a06
bogdantatu10@gmail.com <bogdantatu10@gmail.com>
2021-05-14 19:00:28 +0300

    added login.fxml

--- sef-project/src/main/java/Test/TestLogin.java
+++ sef-project/src/main/java/Test/TestLogin.java
@@ -1,4 +1,22 @@
 package Test;
 
-public class TestLogin {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestLogin extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage.setTitle("Login/Register");
+        stage.setScene(new Scene(root, 600, 800));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
