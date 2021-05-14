2e9dff300dd042fd49d086b6ac13edc3f2f4f834
bogdantatu10@gmail.com <bogdantatu10@gmail.com>
2021-05-15 00:23:44 +0300

    javafx merge in final

---
+++ sefproject/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        //Parent root = FXMLLoader.load(getClass().getResource("/Stages/logreg.fxml"));
+        StackPane root = new StackPane();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Login/Register");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
