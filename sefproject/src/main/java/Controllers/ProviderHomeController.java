989baf9a2d2395a7d229aba2613a8bd52469f801
RaulQX <raul.sarbu@student.upt.ro>
2021-05-15 03:56:01 +0300

    Implemented Provider Database

--- sefproject/src/main/java/Controllers/ProviderHomeController.java
+++ sefproject/src/main/java/Controllers/ProviderHomeController.java
@@ -1,10 +1,36 @@
 package Controllers;
 
+import Components.Provider;
+import Databases.ProviderDTB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class ProviderHomeController {
+
+    private ProviderDTB providerDTB = new ProviderDTB();
+    private Provider currentProvider;
+
+    public ProviderHomeController(String username) {
+
+        providerDTB.readProviders();
+        currentProvider = getProvider(username);
+
+    }
+
+    public Provider getCurrentProvider() {
+        return currentProvider;
+    }
+
+    public Provider getProvider(String username){
+
+        for (Provider i: providerDTB.getData()) {
+            if (i.getName().equals(username)) {
+                return i;
+            }
+        }
+        return null;
+    }
 
     @FXML
     private Text provNameLabel;
