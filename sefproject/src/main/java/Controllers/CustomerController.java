544a2daaa173af0533acbfef42d2ab04bad3a0a7
RaulQX <raul.sarbu@student.upt.ro>
2021-05-17 17:28:59 +0300

    Added CustomerDTB & Testing class

--- sefproject/src/main/java/Controllers/CustomerController.java
+++ sefproject/src/main/java/Controllers/CustomerController.java
@@ -1,4 +1,24 @@
 package Controllers;
 
+import Main.App;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class CustomerController {
+
+    @FXML
+    public Text provNameLabel;
+    public GridPane gamesGridPane;
+
+//    public void setup(String username){
+//        providerDTB = App.getInstance().getProviderDTB();
+//        currentProvider = providerDTB.getCurrentProvider();
+//
+//        provNameLabel.setText(username);
+//        loadData();
+//        resetGameDataFields();
+//        updateGridAndList();
+//    }
+
 }
---
+++ sefproject/src/main/java/Tests/CustomerDTB_Test.java
@@ -0,0 +1,48 @@
+package Tests;
+
+import Components.Customer;
+import Components.Game;
+import Databases.CustomerDTB;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CustomerDTB_Test {
+
+    public static void main(String[] args) {
+
+        Date date = new Date();
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, 12);
+        date = cal.getTime();
+
+
+        Game are = new Game("Alfa", 10, "A t", date, date, true);
+        Game alinare = new Game("beta", 10, "A t ", date, date, true);
+
+        Customer test = new Customer("Alin");
+
+        test.addGame(are);
+        test.addGame(alinare);
+        test.setMoney(2000.2);
+
+        CustomerDTB customerDTB = new CustomerDTB("src/main/resources/Databases/CustomersDTB.json", test.getName());
+
+        customerDTB.setCurrentCustomer(test.getName());
+
+        customerDTB.getCurrentCustomer().addGame(are);
+        customerDTB.getCurrentCustomer().addGame(alinare);
+        customerDTB.getCurrentCustomer().setMoney(200.0);
+        customerDTB.updateDatabase();
+
+        customerDTB.setCurrentCustomer("Oare apar aici");
+        customerDTB.getCurrentCustomer().addGame(are);
+        customerDTB.getCurrentCustomer().setMoney(10000.0);
+        customerDTB.updateDatabase();
+
+        System.out.println(customerDTB.getCurrentCustomer());
+    }
+}
