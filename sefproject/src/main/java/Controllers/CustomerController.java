26d007852f43a0a580e5cc040c037f5ad6f02d91
RaulQX <raul.sarbu@student.upt.ro>
2021-05-18 19:18:50 +0300

    Gui

--- sefproject/src/main/java/Controllers/CustomerController.java
+++ sefproject/src/main/java/Controllers/CustomerController.java
@@ -5,6 +5,8 @@ import Components.Game;
 import Components.Provider;
 import Databases.CustomerDTB;
 import Databases.ProviderDTB;
+import Main.App;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -21,9 +23,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class CustomerController {
-
     CustomerDTB customerDTB;
     Customer currentCustomer;
     ProviderDTB providerDTB;
@@ -35,8 +37,11 @@ public class CustomerController {
     public Text balanceText;
 
     public ListView<String> gameListListView;
-    public ListView<Double> gameListViewPrice;
+    public ListView<String> gameListViewPrice;
     public ListView<String> gameListViewPeriod;
+    public ListView<String> gameTransactionsListView;
+    public ListView<String> gameTransactionsPriceListView;
+    public ListView<String> gameTransactionsTimeLeftListView;
 
     public AnchorPane addMoneyAnchorPane;
     public AnchorPane libraryAnchorPane;
@@ -51,9 +56,9 @@ public class CustomerController {
     public TextField expirationDateTextField;
 
     public void setup(String username){
-        providerDTB = new ProviderDTB("src/main/resources/Databases/ProvidersDTB.json");
-        customerDTB = new CustomerDTB("src/main/resources/Databases/CustomersDTB.json", username);
-        //customerDTB = App.getInstance().getCustomerDTB();
+        providerDTB = App.getInstance().getProviderDTB();
+        customerDTB = App.getInstance().getCustomerDTB();
+
         currentCustomer = customerDTB.getCurrentCustomer();
 
         chooseThisGameButton.disableProperty().bind(gameListListView.getSelectionModel().selectedItemProperty().isNull());
@@ -61,18 +66,13 @@ public class CustomerController {
         loadData();
 //        resetGameDataFields();
         updateGridAndList();
+        setExpirationDateTextFieldListener();
+        setCreditCardTextFieldListener();
+        setGameTransactionsListView();
+
     }
 
-    private void updateGridAndList() {
-
-
-        //System.out.println(gameHolders);
-
-        setGameListListView(providerDTB.getAllGamesFromDTB());
-        setGamesGridPane(currentCustomer.getStringGameArray());
-    }
-
-    // SETTERS
+    /// SETTERS
     public void setGamesGridPane(ArrayList<String> gridPaneStr) {
         int col = 0;
         int row = 0;
@@ -102,28 +102,61 @@ public class CustomerController {
         }
     }
 
+    public void setGameTransactionsListView() {
 
-    public void setGameListListView(ArrayList<Game> gamesFromDTB) {
+        ObservableList<String> observableListTransactions = FXCollections.observableArrayList();
+        ObservableList<String> observableListTransactionsPrice = FXCollections.observableArrayList();
+        ObservableList<String> observableListTransactionsTimeLeft = FXCollections.observableArrayList();
 
+        long diffInMillies;
+        long diff;
+
+        for (Game i: currentCustomer.getLibrary()) {
+            observableListTransactions.add(i.getName());
+            observableListTransactionsPrice.add("$" + i.getPrice());
+
+            if (i.getRent()) {
+                diffInMillies = Math.abs(i.getEndDate().getTime() - i.getStartDate().getTime());
+                diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+                observableListTransactionsTimeLeft.add(diff + " days");
+            }
+            else observableListTransactionsTimeLeft.add("" + '\u221e');
+        }
+
+
+        gameTransactionsListView.getItems().clear();
+        gameTransactionsPriceListView.getItems().clear();
+        gameTransactionsTimeLeftListView.getItems().clear();
+
+        gameTransactionsListView.getItems().addAll(observableListTransactions);
+        gameTransactionsPriceListView.getItems().addAll(observableListTransactionsPrice);
+        gameTransactionsTimeLeftListView.getItems().addAll(observableListTransactionsTimeLeft);
+    }
+
+    public void setGameListListView(ArrayList<Game> games) {
         ObservableList<String> observableListGameName = FXCollections.observableArrayList();
-        ObservableList<Double> observableListGamePrice = FXCollections.observableArrayList();
+        ObservableList<String> observableListGamePrice = FXCollections.observableArrayList();
         ObservableList<String> observableListGamePeriod = FXCollections.observableArrayList();
 
         observableListGameName.removeAll();
         observableListGamePrice.removeAll();
         observableListGamePeriod.removeAll();
 
-        for (Game i: gamesFromDTB) {
+        for (Game game: games) {
+            String name = game.getName();
+            Double price = game.getPrice();
+            String date = game.getStartDate().toString().substring(0,10) + " -> ";
 
-            observableListGameName.add(i.getName());
-            observableListGamePrice.add(i.getPrice());
-            if (i.getRent()) {
-                observableListGamePeriod.add("" + i.getStartDate().toString().substring(0,10) + " -> " +
-                        i.getEndDate().toString().substring(0,10));
+            if (game.getRent()) {
+                date += game.getEndDate().toString().substring(0,10);
             }
-            else observableListGamePeriod.add("" + i.getStartDate().toString().substring(0,10) + " -> " +
-                    "Forever");
+            else {
+                date += "\u221e";
+            }
 
+            observableListGameName.add(name);
+            observableListGamePrice.add("$" + price);
+            observableListGamePeriod.add( date );
         }
 //        System.out.println(observableListGameName);
 //        System.out.println(observableListGamePrice);
@@ -141,12 +174,7 @@ public class CustomerController {
         //gameListListView.getItems().addAll(observableList);
     }
 
-    public void loadData() {
-        customerNameLabel.setText(currentCustomer.getName());
-        balanceText.setText("" + currentCustomer.getMoney());
-
-    }
-
+    /// MOVE BETWEEN PANES
     public void seeLibraryClick() {
         libraryAnchorPane.setVisible(true);
         browseGamesAnchorPane.setVisible(false);
@@ -162,6 +190,7 @@ public class CustomerController {
     }
 
     public void transactionsClick() {
+        setGameTransactionsListView();
         transactionsAnchorPane.setVisible(true);
         libraryAnchorPane.setVisible(false);
         browseGamesAnchorPane.setVisible(false);
@@ -175,33 +204,40 @@ public class CustomerController {
         browseGamesAnchorPane.setVisible(false);
     }
 
-    public void chooseThisGameButton() {
+    public void logOutAction(){
+        App.getInstance().gotoLogout();
+    }
 
+    /// ACTIONS
+    public void chooseThisGameButton() {
         Game currentGame = providerDTB.getAllGamesFromDTB().get(0); //TODO: ceva sa facem sa fie asta initializat
 
-        for (Provider i: providerDTB.getData()) {
-            for (Game j: i.getGames()) {
-                if (j.getName().equals(gameListListView.getSelectionModel().getSelectedItem()))
-                    currentGame = j;
+        for (Provider provider: providerDTB.getData()) {
+            for (Game game: provider.getGames()) {
+                if ( game.getName().equals(gameListListView.getSelectionModel().getSelectedItem()) ) {
+                    currentGame = game;
+                }
             }
         }
 
-        if (currentCustomer.getMoney() < currentGame.getPrice())  {
+        if ( currentCustomer.getMoney() < currentGame.getPrice() )  {
             System.out.println("Not enough money");
             return;
         }
 
+        currentGame.setBought(true);
+        providerDTB.updateDatabase();
         addGameToLibrary(currentGame);
+        updateGridAndList();
 
-
-        String money = "" + (currentCustomer.getMoney() - currentGame.getPrice());
+        String money = "$" + (currentCustomer.getMoney() - currentGame.getPrice());
         money = money.substring(0, money.indexOf('.') + 2);
 
         currentCustomer.setMoney(Double.parseDouble(money));
 
         customerDTB.updateDatabase();
 
-        balanceText.setText("" + currentCustomer.getMoney());
+        balanceText.setText("$" + currentCustomer.getMoney());
 
         browseGamesClick();
 
@@ -210,14 +246,7 @@ public class CustomerController {
 
     }
 
-    public void addGameToLibrary(Game currentGame) {
-        this.currentCustomer.addGame(currentGame);
-        customerDTB.updateDatabase();
-        setGamesGridPane(currentCustomer.getStringGameArray());
-    }
-
     public void addMoneyToAccount() {
-
         String amount = amountTextField.getText();
         String creditCard = creditCardTextField.getText();
         String expirDate = expirationDateTextField.getText();
@@ -248,12 +277,82 @@ public class CustomerController {
         currentCustomer.setMoney(currentCustomer.getMoney() + Double.parseDouble(amount));
 
         customerDTB.updateDatabase();
-        balanceText.setText("" + currentCustomer.getMoney());
+        balanceText.setText("$" + currentCustomer.getMoney());
 
         browseGamesClick();
 
         clearAddMoney();
 
+    }
+
+
+    public void ccvTextFieldChanged(){
+        if ( ccvTextField.getText().length() > 3 ){
+            String text = ccvTextField.getText().substring(0, 3);
+            ccvTextField.setText(text);
+            ccvTextField.positionCaret(3);
+        }
+    }
+
+    // OTHER
+    public void setExpirationDateTextFieldListener(){
+        expirationDateTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            int addFlag = newValue.length() - oldValue.length();
+
+            if ( addFlag > 0 ){
+                if ( newValue.length() > 5 ){
+                    expirationDateTextField.setText(oldValue);
+                    expirationDateTextField.positionCaret(5);
+                    return;
+                }
+                if ( newValue.length() == 2 ){
+                    expirationDateTextField.setText(newValue + "/");
+                    expirationDateTextField.positionCaret(3);
+                }
+            }
+        });
+    }
+
+    public void setCreditCardTextFieldListener(){
+        creditCardTextField.textProperty().addListener((observable, oldValue, newValue) ->{
+            int addFlag = newValue.length() - oldValue.length();
+
+            int[] arr = {4, 9, 14};
+            if ( addFlag > 0 ){
+                if ( newValue.length() > 19 ){
+                    creditCardTextField.setText(oldValue);
+                    creditCardTextField.positionCaret(19);
+                }
+                for (int i = 0 ; i < 3 ; i++){
+                    if ( newValue.length() == arr[i] ){
+                        creditCardTextField.setText(newValue + "-");
+                        creditCardTextField.positionCaret(arr[i]+1);
+                    }
+                    if ( newValue.length() == arr[i]+1 && newValue.charAt(newValue.length()-1) != '-' ) {
+                        creditCardTextField.setText(oldValue + "-" + newValue.charAt(newValue.length()-1));
+                        creditCardTextField.positionCaret(arr[i]+2);
+                    }
+                }
+            }
+        });
+
+
+    }
+
+     private void updateGridAndList() {
+            setGameListListView(providerDTB.getAvailableGamesFromDTB());
+            setGamesGridPane(currentCustomer.getStringGameArray());
+     }
+
+    public void loadData() {
+        customerNameLabel.setText(currentCustomer.getName());
+        balanceText.setText("$" + currentCustomer.getMoney());
+    }
+
+    public void addGameToLibrary(Game currentGame) {
+        this.currentCustomer.addGame(currentGame);
+        customerDTB.updateDatabase();
+        setGamesGridPane(currentCustomer.getStringGameArray());
     }
 
     public void clearAddMoney() {
@@ -262,5 +361,7 @@ public class CustomerController {
         expirationDateTextField.setText("");
         ccvTextField.setText("");
     }
+
+
 
 }
